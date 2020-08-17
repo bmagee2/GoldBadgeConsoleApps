745caa5c6f0a216ca2dc3ddb471aa1288414d0a3
bmagee2 <bamagee2@gmail.com>
2020-08-16 22:47:07 -0400

    tests

--- ChallengeOne-Repository/Menu.cs
+++ ChallengeOne-Repository/Menu.cs
@@ -8,5 +8,26 @@ namespace ChallengeOne_Repository
 {
     public class Menu
     {
+        // Menu Class with properties, constructors, and fields
+
+        // CONSTRUCTORS
+        public Menu() { }
+        public Menu(int itemNumber, string itemName, string itemDescription, decimal itemPrice) // LIST OF INGREDIENTS)
+        {
+            ItemNumber = itemNumber;
+            ItemName = itemName;
+            ItemDescription = itemDescription;
+            ItemPrice = itemPrice;
+            // LIST OF INGREDIENTS
+        }
+
+        // PROPERTIES -- of menu items -- number (int), name (string), description (string), list of ingredients (list or enum?), price (decimal)
+        public int ItemNumber { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public decimal ItemPrice { get; set; }
+        // LIST OF INGREDIENTS -- how to set up a List property or field?
+        //public List<Ingredients> ListOfIngredients { get; set; }
+        //public string _goatName;
     }
 }
---
+++ ChallengeOne-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChallengeOne-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChallengeOne-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("161cf1f6-8782-4b53-a32f-947bb97c7057")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
