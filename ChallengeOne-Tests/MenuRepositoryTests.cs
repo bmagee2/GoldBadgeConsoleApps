245f94d6b26f80ad2f4fd0e90ee8e0838a96280e
bmagee2 <bamagee2@gmail.com>
2020-08-17 14:53:27 -0400

    tests

---
+++ ChallengeOne-Tests/MenuRepositoryTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using ChallengeOne_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChallengeOne_Tests
+{
+    [TestClass]
+    public class MenuRepositoryTests
+    {
+
+        private MenuRepository _repo;
+        private Menu _menu;
+
+        [TestMethod]
+        public void GetListOfMenuItems()    // Should get all menu items
+        {
+            // ARRANGE
+
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [TestMethod]
+        public void GetMenuItemByName()     // Should get one specific menu item
+        {
+            // ARRANGE
+
+
+            // ACT
+
+            // ASSERT
+        }
+
+        [TestMethod]
+        public void AddNewMenuItemToList()      // Should 
+        {
+
+        }
+
+        [TestMethod]
+        public void DeleteMenuItem()            // Should return true
+        {
+            // Arrange
+            // this will be replaced with our [TestInitialize] method
+            // Arrange()
+
+            // Act
+            bool delete = _repo.DeleteExistingContent(_content);
+
+            // Assert
+            Assert.IsTrue(delete);
+        }
+    }
+}
