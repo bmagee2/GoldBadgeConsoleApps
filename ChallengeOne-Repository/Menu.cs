eb7f02e0ed04ea678a25be1fdd55e2de07b30cdf
bmagee2 <bamagee2@gmail.com>
2020-08-16 23:10:27 -0400

    console/tests 3

--- ChallengeOne-Repository/Menu.cs
+++ ChallengeOne-Repository/Menu.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace ChallengeOne_Repository
 {
     // POCO -- single responsiblity object that holds data
+    // test change
     public class Menu
     {
         // Menu Class with properties, constructors, and fields
---
+++ ChallengeThree-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ChallengeThree-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ChallengeThree-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f021a02-1f1f-41ed-bb50-4a8fadc5c4e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
